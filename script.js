40831045d19d19af6db305299fa8263e94b87055
cocojac <cocojac@fab.mil.br>
2025-11-19 22:08:34 -0300

    Correção Readme

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
+    //alert("1");
     const toggleMenu = () => {
         const menu = document.getElementById('navMenu');
         if(menu)menu.classList.toggle('active')
     };
-
+    //alert("2");
     window.toggleMenu = toggleMenu;
-
+    //alert("3");
     const scrollToSection = (sectionId) =>{
         const section = document.getElementById('sectionId');
         if(!section) return;
@@ -16,8 +17,9 @@ document.addEventListener('DOMContentLoaded', () =>{
         const menu = document.getElementById('navMenu');
         if(menu) menu.classList.remove('active');
     }
-
+    //alert("4");
     const handleSubmit = (event) => {
+        alert("Aqui");
         event.preventDefault();
         const form = document.getElementById('volunteerForm');
         if(!form) return;
@@ -33,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             form.dataset.submmiting = 'false';
             return;
         }
-
+ alert("5");
         const formData = {
             nome: form.nome.value.trim(),
             email: form.email.value.trim(),
@@ -45,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             cidade: form.cidade.value.trim(),
             estado: form.estado.value.trim()
         };
-
+ alert("6");
         let voluntarios = JSON.parse(localStorage.getItem('voluntarios') || []) ;
         voluntarios.push(formData);
         localStorage.setItem('voluntarios',JSON.stringify(voluntarios));
@@ -64,11 +66,11 @@ document.addEventListener('DOMContentLoaded', () =>{
         exibirVoluntarios();
 
     };
-
-    const form = document.getElementById('voluntarios');
+     //alert("7");
+    const form = document.getElementById('voluntarioss');
     if (form) form.addEventListener('submit',handleSubmit);
     const exibirVoluntarios = () =>{
-        const voluntarios=JSON.parse(localStorage.getItem('voluntarios') || []);
+        const voluntarios=JSON.parse(localStorage.getItem('voluntarioss') || []);
         const tabelaContainer = document.getElementById('tabelaVoluntarios');
         if (!tabelaContainer) return;
         if (voluntarios.lenght === 0 ){
@@ -79,15 +81,15 @@ document.addEventListener('DOMContentLoaded', () =>{
         html += '<tr><th>Nome</th></tr>';
         html += '<tr><th>Email</th></tr>';
         html += '<tr><th>telefone</th></tr>';
-        voluntarios.array.forEach(v => {
+        voluntarioss.array.forEach(v => {
             html += '<tr>'
             html += '<td>$v.nome</td>'
             html += '<td>$v.email</td>'
             html += '<td>$v.telefone</td>'
             html += '</tr>'
         });
-        html += '</table>'
-
- 
+        html += '</table>' 
     }
+
+    
 });
